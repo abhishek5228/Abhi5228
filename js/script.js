9ab38c05881e9a2f9ef3b562dc3ca409e66a536e
Abhishek Kumar <59703066+0031abbhishek@users.noreply.github.com>
2021-04-29 21:41:10 +0530

    Javascript File

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling
+
+$('a[href*="#"]').click(function(e) {
+  e.preventDefault();
+  var target = this.hash;
+  $target = $(target);
+
+  $('html, body').stop().animate({
+    'scrollTop': $target.offset().top
+  }, 900, 'swing', function () {
+    window.location.hash = target;
+  });
+});
